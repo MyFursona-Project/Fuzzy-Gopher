c8e7904bf431861d0a2bf9fc4f06858421e4ef2f
Damon Leven <dleven@protonmail.com>
2022-07-21 16:06:21 +0200

    small DB model improvements

--- database/models.go
+++ database/models.go
@@ -6,31 +6,34 @@ type User struct {
 	UserID    string `gorm:"primaryKey"`
 	EMail     string
 	Name      string
-	Password  string
 	CreatedAt time.Time
 	locked    bool
 	// foreign keys
-	Verify           []Verify  `gorm:"foreignKey:UserID"`
 	ProfileUserID    []Profile `gorm:"foreignKey:UserID"`
 	ProfileName      []Profile `gorm:"foreignKey:Name"`
 	ProfileCreatedAt []Profile `gorm:"foreignKey:CreatedAt"`
 	Fursona          []Fursona `gorm:"foreignKey:UserID;foreignKey:OwnerID"`
 	Like             []Like    `gorm:"foreignKey:UserID"`
-}
-type Verify struct {
-	ID        string `gorm:"primaryKey"`
-	UserID    string // User.UserID
-	EMailCode string
+	Social           []Social  `gorm:"foreignKey:UserID"`
 }
 type Profile struct {
-	UserID    string    `gorm:"primaryKey"` // -> User.UserID
-	Name      string    // -> User.Name
-	CreatedAt time.Time // -> User.CreatedAt
-	Location  string
-	State     string
-	Gender    string
-	Sexuality string
+	UserID        string    `gorm:"primaryKey"` // -> User.UserID
+	Name          string    // -> User.Name
+	CreatedAt     time.Time // -> User.CreatedAt
+	Location      string
+	State         string
+	Gender        string
+	Sexuality     string
+	PrimarySonaID string
+	Adult         bool // if true the user can see nsfw
 }
+type Social struct {
+	ID       string
+	UserID   string // -> User.UserID
+	SiteName string
+	URL      string
+}
+
 type Fursona struct {
 	FursonaID string `gorm:"primaryKey"`
 	OwnerID   string // -> User.UserID
@@ -55,4 +58,14 @@ type Like struct {
 	LikeID    string `gorm:"primaryKey"`
 	UserID    string // -> User.UserID
 	FursonaID string // -> Fursona.FursonaID
+}
+
+type FursonaAnon struct {
+	FursonaID string `gorm:"primaryKey"`
+	Name      string
+	Gender    string
+	Sexuality string
+	// foreign keys
+	FursonaMedia []FursonaMedia `gorm:"foreignKey:FursonaID"`
+	Like         []Like         `gorm:"foreignKey:FursonaID"`
 }
